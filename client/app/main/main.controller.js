2ccd4a868a9c1d8734ed5c5e01db0f723f2f02ae
Konviser <svetsapelova@gmail.com>
2014-09-06 10:38:13 +0200

    add google maps

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('pixformanceHomeworkApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(40.0000, -98.0000),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
 
-  });
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var infoWindow = new google.maps.InfoWindow();
+
+  }]);
