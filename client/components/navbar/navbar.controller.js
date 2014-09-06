81163059c9af51b8e98d82ab922ee7c9bc08d1a1
Konviser <svetsapelova@gmail.com>
2014-09-06 09:24:25 +0200

    scaffold app with yeoman

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('pixformanceHomeworkApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
